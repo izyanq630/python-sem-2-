6eb1f8b0428728590f7ee5043f8cbee8546a00bf
izyanq630 <izyanq@eng.rizvi.edu.in>
2026-01-20 09:59:34 +0530

    Add files via upload

---
+++ EXP2.py
@@ -0,0 +1,15 @@
+r= int(input("Enter radius:"))
+area = 3.14*r*r
+print("Print area of the circle:",area)
+
+
+l= int(input("enter length:"))
+b= int(input("enter breadth:"))
+area= l*b
+print("Area of the rectangle:",area)
+
+
+b= int(input("enter base:"))
+h= int(input("enter height:"))
+area= 0.5*b*h
+print("Area of the triangle:",area)
